fbccf5e2f1de6132e8b67f2228ccb009bb738b9b
Henrik <jonssonhenrik@hotmail.com>
2020-12-07 12:55:19 +0100

    Comment controller and 2 views

---
+++ BlogoSphere/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogoSphere.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace BlogoSphere.Controllers
+{
+
+    public class CommentController : Controller
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
+        public ActionResult Viewcomment()
+        {
+            var commentList=
+            return View();
+        }
+        public ActionResult CreatCommentList() => RedirectToAction("Viewcomment", "Comments");
+        public ActionResult CreatComment()
+        {
+            return View();
+           
+        }
+    }
+}
