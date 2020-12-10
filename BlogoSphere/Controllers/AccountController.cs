b83f5ba1a9af117ff6caf111f63b245bed94d049
Sonia <sonya.alle@hotmail.com>
2020-12-10 11:52:28 +0100

    Details, delete and edit done

--- BlogoSphere/Controllers/AccountController.cs
+++ BlogoSphere/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace BlogoSphere.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, FirstName = "n/a", LastName = "n/a" };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
