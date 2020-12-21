d914702601bbfeb9860a3251926f2b9339d74882
Richard Lindholm <richard@thelindholm.se>
2020-12-21 14:36:33 +0100

    Small fixes to make it work

--- BlogoSphere/Controllers/PostController.cs
+++ BlogoSphere/Controllers/PostController.cs
@@ -63,8 +63,6 @@ namespace BlogoSphere.Controllers
         public ActionResult List(List<Post> posts)
         {
             ViewBag.BlogId = posts.FirstOrDefault().BlogId;
-            if (model.Count == 0 || model == null)
-                return View();
 
             return View(posts);
 		}
